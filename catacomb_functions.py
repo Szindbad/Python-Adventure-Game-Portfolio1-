c4bfa355ea1480aa2c1e8fdea1b41ae089d5b5f7
Tamas Juhasz <119873300+Szindbad@users.noreply.github.com>
2023-06-30 16:31:12 +0200

    Add files via upload

--- catacomb_functions.py
+++ catacomb_functions.py
@@ -1,105 +1,75 @@
-
 import screens
 import inventory
-from inventory import Obstacle
-import main
 import random
-from main import player_instance
+import main
 
+screen_str, random_obstacle = inventory.Obstacle.create_random_world1_screen()
 
-def create_random_world1_screen():
-    global random_obstacle  # declare the variable as global inside the function
-    """creates a random screen, importing from the screens file"""
-    title = random.choice(screens.titles)
-    description = random.choice(screens.description)
-    obstacle = random.choice(screens.obstacles)
-    health = random.randint(1, 5)
-    obstacle_type = random.choice(screens.obstacle_types)
-    doors = random.sample(screens.doors, 4)
-    screen_id = random.randint(1000, 9999)
-    damage = random.randint(1, 20)
-    screens.random_screens_id.append(screen_id)  # append screen_id to the list
-    # create a string with the screen information
-    obstacle_screen_str = f"You are in {title}. This place is {description} You see a {obstacle}, " \
-                          f"it has {health} life points." \
-                          f"It seems like it is a {obstacle_type}. " \
-                          f"What do you do?"
-    random_obstacle = Obstacle(obstacle, obstacle_type, 1, health, damage)
-    # return the screen string and the Obstacle object
-    return obstacle_screen_str, random_obstacle
-
-
-# obstacle instances related things, maybe this is not the best place for them, but couldn't put them to inventory #
-# Obstacle instances handling#
-# Call the create_random_world1_screen function to get the screen string and the Obstacle object
-screen_str, random_obstacle = create_random_world1_screen()
-# Create an instance of the Obstacle class based on the random_obstacle variable
-if random_obstacle:
-    obstacle_instance = Obstacle(random_obstacle.name, random_obstacle.type, random_obstacle.level,
-                                 random_obstacle.health, random_obstacle.damage)
-else:
-    # Handle the case where random_obstacle is None
-    obstacle_instance = None
+player_instance = inventory.Player()
 
 
 # after an unsuccessful attack or befriend, this gives the player the choice to do an action again
 def another_chance():
-    global another_chance_action_choice
     another_chance_action_str = input(
-        'It seems like this creature is tougher than it looks, you can choose to attack or befriend them again' "\n")
+        'It seems like this creature is tougher than it looks, but do not worry! You can choose to attack or befriend them again' "\n")
     flag = False
     while not flag:
         if another_chance_action_str.lower() not in main.action_choices:
-            print('Invalid choice. Please choose "attack" or "befriend".' "\n")
+            print('Invalid choice. Please choose "attack" or "befriend".\n')
             another_chance_action_str = input('Which will it be? ' "\n")
         else:
             if another_chance_action_str.lower() == main.action_choices[0]:
-                print(attack_world1())
+                print(attack_world1(random_obstacle=random_obstacle))
             elif another_chance_action_str.lower() == main.action_choices[1]:
-                print(befriend_world1())
+                print(befriend_world1(random_obstacle=random_obstacle))
             flag = True
 
 
 # action functions related to obstacles
+def befriend_world1(**kwargs):
+    random_obstacle = kwargs.get('random_obstacle', None)
+
+    if random_obstacle is None:
+        print("Emptiness stares back at you.\n")
+        return
+
+    random_number = random.randint(0, 5)
+    if random_number == 0:
+        print("You became friends with " + random_obstacle.name + ", how lovely!" "\n")
+    elif random_number == 1:
+        print("You became associates with " + random_obstacle.name + ", how interesting!" "\n")
+    elif random_number == 2:
+        print(
+            "You know each other from now on! Awkwardly wave next time you see " + random_obstacle.name + " on the bus!" "\n")
+    else:
+        print("They don't like you!" "\n")
+        player_instance.obstacle_attack()
+
 
 def get_action_choice():
     action_choices = ['attack', 'befriend']
     while True:
-        choice_action = input('Which will it be? ' "\n")
+        choice_action = input('Which will it be?\n')
         if choice_action.lower() not in action_choices:
-            print('Invalid choice. Please choose "attack" or "befriend".' "\n")
+            print('Invalid choice. Please choose "attack" or "befriend".\n')
         elif choice_action.lower() == action_choices[0]:
-            screen_str, random_obstacle = create_random_world1_screen()
-            attack_world1()
+            attack_world1(random_obstacle=random_obstacle)
             return
         elif choice_action.lower() == action_choices[1]:
-            print(befriend_world1())
+            another_chance()
             return
 
 
-def attack_world1():
+def attack_world1(**kwargs):
+    random_obstacle = kwargs.get('random_obstacle')
     attack_power = random.randint(1, 5)
 
-    if attack_power >= obstacle_instance.health:
-        print("You vanquished your enemy!" "\n")
-        screens.defeated_obstacles.append(obstacle_instance)
+    if attack_power >= random_obstacle.health:
+        print("You vanquished your enemy!\n")
+        screens.defeated_obstacles.append(random_obstacle.name)
     else:
         print(random.choice(screens.attack_sentences))
         player_instance.obstacle_attack()
-
-
-def befriend_world1():
-    random_number = random.randint(0, 5)
-
-    if random_number == 0:
-        print("You became friends with " + obstacle_instance.name + ", how lovely!" "\n")
-    elif random_number == 1:
-        print("You became associates, how interesting!" "\n")
-    elif random_number == 2:
-        print("You know each other from now on! Awkwardly wave next time you see each other on the bus!" "\n")
-    else:
-        print("They don't like you!" "\n")
-        print(player_instance.obstacle_attack())
 
 
 def which_door_opens():
@@ -124,7 +94,6 @@ def which_door_opens():
             flag = True  # set the flag to True to exit the while loop
 
     while loop == 1:
-        print(create_random_world1_screen())
         break
 
 
@@ -137,21 +106,9 @@ screen = {
 }
 
 
-# after certain number of loops, this should happen
-def level_up_obstacles():
-    """Levels up the obstacles"""
-    global random_obstacle
-    count_list_elements = lambda x: len(x)
-    for random_obstacle in screens.defeated_obstacles:
-        if count_list_elements(screens.defeated_obstacles) > inventory.Player.game_level:
-            inventory.Obstacle.level += 4
-            print('You progress deeper into the unknown, get ready for some tougher enemies!' "\n")
-    return random_obstacle.level
-
-
 def game_over():
     """if player is dead, game end"""
-    if inventory.Player.hp == 0:
+    if inventory.Player.hp <= 0:
         print('''
         ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
         ███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
